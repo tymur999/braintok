386a13a89694a7f974c3a86e114f535a5220eae9
tymur999 <tymur.arsent@gmail.com>
2024-10-19 19:39:51 -0400

    before side

--- src/app/data/notes.tsx
+++ src/app/data/notes.tsx
@@ -1,10 +1,3 @@
 // src/data/notes.ts
 
-export const notes: string[] = [
-    "The average attention span is 8.25 s",
-    "E = mc^2",
-    "We are a team of dreamers!",
-    "Our names are Kaz, Tymur, Daniil, and Mark!",
-    "LET'S GO TECH",
-    "In my free time, I do poster design for my dorm room",
-];
+export const notes: string[] = ["basics of mathematical proofs", "fundamentals of propositional logic", "direct proof method", "logical expressions using quantifiers", "problem-solving skills through logic puzzles"];
--- src/app/landing/time.tsx
+++ src/app/landing/time.tsx
@@ -7,6 +7,15 @@ import { useEffect, useState } from "react";
 // import getSubtask from "@/api/openAI/openAI-API";
 // import CanvasClass from "@/api/canvas.class";
 // import getSubtask from "../../api/openAI/openAI-API";
+const questions = [
+    "What topics are covered in day 4 nested quantifiers?",
+    "What key concepts are covered in the introduction to proofs material?",
+    "What are the main topics covered in day1 propositional logic?",
+    "What are the concepts covered in the day 2: puzzles and propositions?",
+    "What concepts are covered in day-3-predicates-and-quantifiers?",
+    "What key concepts are covered in introduction to proofs?"
+];
+const randomIndex = Math.trunc(Math.random() * questions.length);
 
 export function Time(props: {seconds: number, setIndex: (index: number) => void}) {
     const [expectedDate] = useState(Date.now()/1000+props.seconds);
@@ -23,7 +32,6 @@ export function Time(props: {seconds: number, setIndex: (index: number) => void}
     //     console.log(subtask);
     // }
     // hello();
-
 
     useEffect(() => {
         decreaseClock();
@@ -45,7 +53,7 @@ export function Time(props: {seconds: number, setIndex: (index: number) => void}
         className="absolute top-0 flex flex-col bg-[#ff5757] h-full w-full items-center justify-center
         from-[#ff5757] to-[rgba(56,56,56,.6)] bg-gradient-to-b from-90%">
         <h1 className="text-2xl text-center absolute top-[15%] font-bold">
-            What is covered in the "day 4 nested quantifiers" from the course?
+            {questions[randomIndex]}
         </h1>
         <h1 className="absolute text-9xl font-bold" style={{textShadow: "5px 5px 5px 5px black"}}>
             {
